8a1f2969b920bb90143222629122222c3c7a9182
Mathieu Thiry <promathieuthiry@gmail.com>
2024-06-30 18:06:01 +0200

    Add footer

--- markdown_editor/src/components/App.tsx
+++ markdown_editor/src/components/App.tsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 import "./App.css";
 import { MarkdownEditor } from "./markdownEditor/MakdownEditor";
 import pullRequestMarkdown from "./markdownEditor/data";
+import Footer from "./footer/Footer";
 
 function App() {
   const [content, setContent] = useState(pullRequestMarkdown);
 
   return (
-    <>
-      <h1>Markdown Editor</h1>
-      <MarkdownEditor content={content} setContent={setContent} />
-    </>
+    <div className="content">
+      <div className="markdown_container">
+        <h1>Markdown Editor</h1>
+        <MarkdownEditor content={content} setContent={setContent} />
+      </div>
+      <div className="separator" />
+      <Footer />
+    </div>
   );
 }
 
---
+++ markdown_editor/src/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import "./FooterStyle.css"; // Assuming you have a CSS file for styling
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Made with{" "}
+      <span role="img" aria-label="love">
+        ❤️
+      </span>{" "}
+      by{" "}
+      <a href="https://github.com/promathieuthiry" target="_blank">
+        promathieuthiry
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
